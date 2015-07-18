ba9d720f3a472070a960d3e37025af8a809d0fb5
inakihdez <ihdezromeo@hotmail.com>
2015-07-19 00:27:58 +0200

    Update clean-blog.js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -86,6 +86,16 @@ $('#name').focus(function() {
  // *
  // * http://ReactiveRaven.github.com/jqBootstrapValidation/
  
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
 
 (function( $ ){
 
